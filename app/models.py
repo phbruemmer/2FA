fabb9f5d10c16273506b63b1d7567c43f9274d4b
phbruemmer <phbruemmer@gmail.com>
2024-04-27 00:23:34 +0200

    - prepared database class for User registration - not finished -> Add verification

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(
+        max_length=20,
+        help_text='Defines Username')
+    user_email = models.EmailField(
+        help_text='Defines Email')
+    user_password = models.CharField(
+        max_length=60,
+        help_text='Defines User Password - Encrypted and verified')
+
