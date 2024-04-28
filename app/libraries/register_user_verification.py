6d9e15dbeca7b9126079a4cabf8e09eb8e3dd728
phbruemmer <phbruemmer@gmail.com>
2024-04-28 20:29:02 +0200

    - Created register_user_verification.py - Usage:     - This script will create a custom URL to verify the user     - NOT FINISHED:         - We have to check if the custom URL already exists even though it is almost impossible to get twice the same URL         - Only the same USER can get this URL twice (basicall impossible)

--- app/libraries/register_user_verification.py
+++ app/libraries/register_user_verification.py
@@ -11,7 +11,7 @@ import random
 import string
 
 
-def create_custom_str():
+def create_custom_url(username):
     def create_random():
         char = ''
         num = random.randint(0, 1)
@@ -20,26 +20,25 @@ def create_custom_str():
         elif num == 1:
             char = string.digits[random.randint(0, len(string.digits) - 1)]
         return char
-    final_str = ""
-    for i in range(random.randint(12, 18)):
-        final_str += create_random()
-    return final_str
 
+    def create_custom_str():
+        final_str = ""
+        for i in range(random.randint(12, 18)):
+            final_str += create_random()
+        return final_str
 
-def create_custom_url(username):
-    custom_url = '/verify/'
-    custom_url += username + '/' + create_custom_str()
+    custom_url = '../verify/'
+    custom_str = create_custom_str()
 
-    return custom_url
+    custom_url += username + '/' + custom_str + '/'
+
+    return [custom_url, custom_str]
 
 """
-
 - save link in db (username, custom_link)
 - move user credentials from temp to main
     - In case of no verification:
         - delete user in temp after 20 min
     - In case of no received message (resend email on btn-click)
         - delete custom link from db and create new one
-
-
 """
