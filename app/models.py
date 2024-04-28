1c18dcbbfed2c53ebdf1e378e6c5775a48dd85ab
phbruemmer <phbruemmer@gmail.com>
2024-04-28 20:25:45 +0200

    - changed models.py - created TempUser - created RegisteredUser - TempUser will move its data to RegisteredUser after User verification - created TempURL - changed TempURL

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 
-class User(models.Model):
+class TempUser(models.Model):
     username = models.CharField(
         max_length=20,
         help_text='Defines Username')
@@ -11,6 +12,24 @@ class User(models.Model):
         max_length=60,
         help_text='Defines User Password - Encrypted and verified')
 
-    def str(self):
-        return self.username
+
+class RegisteredUser(models.Model):
+    username = models.CharField(
+        max_length=20,
+        help_text='Defines Username')
+    user_email = models.EmailField(
+        help_text='Defines Email')
+    user_password = models.CharField(
+        max_length=60,
+        help_text='Defines User Password - Encrypted and verified')
+    registration_date = models.DateTimeField(default=timezone.now, help_text='Registration Date')
+
+
+class TempURL(models.Model):
+    verification_code = models.CharField(max_length=32,
+                                         help_text='Verification Code to verify the user')
+    username = models.CharField(max_length=20)
+
+
+
 
