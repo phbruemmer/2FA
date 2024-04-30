80d86cee07c1c246d23ea5440a5268c5cb8f6aa5
phbruemmer <phbruemmer@gmail.com>
2024-04-30 23:57:05 +0200

    - Added hash_function.py to libraries

---
+++ app/libraries/hash_function.py
@@ -0,0 +1,10 @@
+
+"""
+- - - - - - - - - - -
+Hashfunction - Encryption
+- - - - - - - - - - -
+
+Hashfunction are functions to encrypt data and to securely decrypt data.
+The function creates a code (for example 256 Bits long) and encrypts the data with it.
+The Code is always the same for the same text but will change if you change something.
+"""
