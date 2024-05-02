37451c9ba6fefc64c4fde20a12c84c5ecc93f3c2
phbruemmer <phbruemmer@gmail.com>
2024-05-02 21:27:50 +0200

    - hash_function.py started -> continue later today

--- app/libraries/hash_function.py
+++ app/libraries/hash_function.py
@@ -8,3 +8,20 @@ Hashfunction are functions to encrypt data and to securely decrypt data.
 The function creates a code (for example 256 Bits long) and encrypts the data with it.
 The Code is always the same for the same text but will change if you change something.
 """
+
+
+def encrypt_data(data):
+    data_ascii_list = []
+    data_binary_list = []
+    print(data)
+    for i in data:
+        i = ord(i)
+        data_ascii_list.append(i)
+        data_binary_list.append(bin(i))
+        # print(i)
+        # print(bin(i))
+    print(data_binary_list)
+
+
+encrypt_data('TestNachricht')
+
