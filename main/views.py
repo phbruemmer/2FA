24041cb535bfb0ada5b6fbd5dd665b2a0ab7c263
phbruemmer <phbruemmer@gmail.com>
2024-05-02 21:41:44 +0200

    - Database changes - main/views.py changed

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def main_view(request):
-    return render(request, 'templates/verify.html')
+    return render(request, 'templates/mainAppTemplate/main.html')
