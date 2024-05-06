fbc6d25e842eb69e9fac92641e0f896d63b1e904
phbruemmer <phbruemmer@gmail.com>
2024-05-06 15:10:54 +0200

    - edited hash_function.py     - sorting and commenting ...

--- app/libraries/hash_function.py
+++ app/libraries/hash_function.py
@@ -10,7 +10,7 @@ The Code is always the same for the same text but will change if you change some
 """
 
 
-DEBUG = True
+DEBUG = False
 
 
 def get_bin(binary_data):
@@ -99,108 +99,68 @@ def compare(data):
 
 
 def sha256(data):
-    """print(data)
-    binary_list = []
-    # Convert Data to Binary using ASCII
-    for i in data:
-        binary_data = bin(ord(i))
-        binary_list.append(binary_data)
-    # Get bitwise length of data input
-    data_binary_len = len(binary_list) * 8
+    def sha_prep():
+        print(data)
+        binary_list = []
 
-    # calculate zeros to append
-    k_bit_len = 896 - (data_binary_len + 1)
+        # Convert Data from ASCII to Binary in 8 Bit chunks
+        for i in data:
+            binary_data = format(ord(i), '08b')
+            binary_list.append(binary_data)
+            if DEBUG:
+                print(binary_data)
 
-    binary_zero_bits = 0b1
+        # Length of the raw data in binary (bitwise)
+        CONST_DATA_LEN = len(binary_list) * 8
 
+        # Append single one to the end of the data chunks
+        binary_list.append(format(128, '08b'))
+        data_binary_len = len(binary_list)
 
-    masked_number = binary_zero_bits & ((1 << k_bit_len) - 1)
-    padded_number = masked_number << (k_bit_len - binary_zero_bits.bit_length())
-    print(bin(padded_number))
-
-
-    print(binary_zero_bits)
-    """
-    print(data)
-
-    binary_list = []
-
-    for i in data:
-        binary_data = format(ord(i), '08b')
-        binary_list.append(binary_data)
         if DEBUG:
-            print(binary_data)
+            print(f'DEBUG - data_binary_len -> {data_binary_len}')
+        # Get Data length in bits
+        data_bit_len = data_binary_len * 8
+        if DEBUG:
+            print(f'DEBUG - data_bit_len -> {data_bit_len}')
 
-    CONST_DATA_LEN = len(binary_list) * 8
+        # i equals the size of the
+        i = 512
+        CONST_I = 512
 
-    # Convert Data to Binary using ASCII
-    # Get bitwise length of data input
-    # Append single one at the end
-    binary_list.append(format(128, '08b'))
-    data_binary_len = len(binary_list)
+        while data_bit_len + 64 >= i:
+            i += CONST_I
 
-    print(binary_list)
+        count = (i - 64) - data_bit_len
+        if DEBUG:
+            print(f'DEBUG - count -> {count}')
 
-    print(f'DEBUG - data_binary_len -> {data_binary_len}')
-    data_bit_len = data_binary_len * 8
-    print(f'DEBUG - data_bit_len -> {data_bit_len}')
+        while (count % 8) == 0 and not count == 0:
+            count -= 8
+            # print(count)
+            binary_list.append(format(0, '08b'))
 
-    i = 512
-    CONST_I = 512
+        if DEBUG:
+            print(f'DEBUG - binary_list -> {binary_list}')
+            print(f'DEBUG - binary_list_len -> {len(binary_list) * 8}')
 
-    while data_bit_len + 64 >= i:
-        i += CONST_I
-    print(i)
+        # # # # # # # # # # # # # # # # # #
+        # ADD LENGTH AT THE END (64 BIT)  #
+        # # # # # # # # # # # # # # # # # #
 
-    count = (i - 64) - data_bit_len
-    print(f'DEBUG - count -> {count}')
+        data_len = format(CONST_DATA_LEN, '064b')
+        k_len_binary_length = len(data_len)
 
-    while (count % 8) == 0 and not count == 0:
-        count -= 8
-        #print(count)
-        binary_list.append(format(0, '08b'))
+        for i in range(0, k_len_binary_length, 8):
+            binary_list.append(data_len[i:i + 8])
+        print(binary_list)
 
-    print(f'DEBUG - binary_list -> {binary_list}')
-    print(f'DEBUG - binary_list_len -> {len(binary_list) * 8}')
-
-    """if compare(binary_list):
-        print('Nothing wrong here i guess')
-    else:
-        print('Something is wrong here')"""
-
-    # # # # # # # # # # # # # # # # # #
-    # ADD LENGTH AT THE END (64 BIT)  #
-    # # # # # # # # # # # # # # # # # #
-
-    data_len = format(CONST_DATA_LEN, '064b')
-    k_len_binary_length = len(data_len)
-
-    for i in range(0, k_len_binary_length, 8):
-        binary_list.append(data_len[i:i + 8])
-
-    print(binary_list)
-
-"""
-    while not (k_len_binary_length % 8) == 0:
-        k_len_binary_length += 1
-
-    k_zero_bytes = round((64 - k_len_binary_length) / 8)
-
-    for i in range(0, k_zero_bytes):
-        binary_list.append(format(0, '08b'))
-
-    chunks = []
-    print(data_len)
-    for i in range(len(data_len), 0, -8):
-        chunk = data_len[i:i + 8]
-        chunks.append(chunk)
-    print(chunks)
-"""
+    sha_prep()
 
 
 
 
-sha256('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+sha256('abc')
 
 
 
