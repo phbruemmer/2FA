4bad707d8ecaf2f5f9912bd78c684b16be28cc0f
phbruemmer <phbruemmer@gmail.com>
2024-05-29 21:40:46 +0200

    - Edited register_user_verification.py     - Added new url-creator function

--- app/libraries/register_user_verification.py
+++ app/libraries/register_user_verification.py
@@ -34,14 +34,30 @@ def create_custom_url(username):
 
     return [custom_url, custom_str]
 
-"""
-- save link in db (username, custom_link)
-- move user credentials from temp to main
-    - In case of no verification:
-        - delete user in temp after 20 min
-    - In case of no received message (resend email on btn-click)
-        - delete custom link from db and create new one
-"""
+
+def create_recovery_code(user_id):
+    user_id = str(user_id)
+    def create_random():
+        char = ''
+        num = random.randint(0, 1)
+        if num == 0:
+            char = string.ascii_letters[random.randint(0, len(string.ascii_letters) - 1)]
+        elif num == 1:
+            char = string.digits[random.randint(0, len(string.digits) - 1)]
+        return char
+
+    def create_custom_str():
+        final_str = ""
+        for i in range(random.randint(24, 32)):
+            final_str += create_random()
+        return final_str
+
+    custom_url = '../recovery/'
+    custom_str = create_custom_str()
+
+    custom_url += user_id + '/' + custom_str + '/'
+
+    return [custom_url, custom_str]
 
 
 def create_code():
@@ -49,3 +65,7 @@ def create_code():
     for i in range(0, 6):
         code += string.digits[random.randint(0, 9)]
     return code
+
+
+if __name__ == "__main__":
+    print(create_recovery_code(12353425))
