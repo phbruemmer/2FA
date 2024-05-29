6b42fd836484c4e16495fd8ba9e8393acceab8ca
phbruemmer <phbruemmer@gmail.com>
2024-05-29 21:45:08 +0200

    - Edited urls.py     - Added password recovery urls

--- app/urls.py
+++ app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', views.login),
     path(r'login/verify_code/<str:username>/', views.handle_login_code, name='handle_login_code'),
     path(r'login/reset_password/', views.reset_password, name='reset_password'),
+    path(r'recover/<str:user_id>/<str:code>/', views.change_password, name='change_password')
 ]
